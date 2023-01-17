5c554f539feeaa952ce3c101db16ff26c3d069a6
brandonsams <brandonkeithsams@gmail.com>
2023-01-17 05:08:50 +0000

    started building scaffolding for primes

---
+++ applications/math/prime/prime.py
@@ -0,0 +1,29 @@
+class PrimeChecker:
+
+	def __init__(self):
+		pass
+
+	@classmethod
+	def is_prime(cls, number):
+		pass
+
+	@classmethod
+	def decompose(cls, number):
+		pass
+
+class PrimeFactorDecomposition:
+	def __init__(self, factor_list):
+		pass
+
+class PositiveNaturalNumber:
+	def __init__(self, num):
+		pass
+
+class PrimeNumber:
+	def __init__(self):
+		pass
+
+class CompositeNumber:
+	def __init__(self);
+		pass
+
